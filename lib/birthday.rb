a6967d9c0325f22ca02c3b476850cfb1adf2c4cb
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-23 20:25:37 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,6 @@ require 'pry'
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     binding.pry
-
      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
    end
  end
